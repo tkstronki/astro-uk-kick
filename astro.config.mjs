bebdfa0bb09bc7baba855c1481cb333d6114f0c0
tkstronki <tkstronki@gmail.com>
2023-09-24 13:37:46 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    compressHTML: false,
+    // build: {
+    //     assetsPrefix: 'https://page-url'
+    //   },
+    // site: 'https://username.github.io',
+    // base: '/uk-kick',
+});
