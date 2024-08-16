5fc68283541a0e243138e40c0145cc5cb2da4c4a
tkstronki <tkstronki@gmail.com>
2024-08-16 21:34:11 +0200

    update: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+// set if you have trouble loading images
+// import { defineConfig, squooshImageService } from 'astro/config';
+
 // https://astro.build/config
 export default defineConfig({
+
+    // https://docs.astro.build/pl/guides/images/#default-image-service
+    // https://docs.astro.build/pl/guides/images/#configure-squoosh
+    // set if you have trouble loading images
+    // image: {
+    //     service: squooshImageService(),
+    // },
+
     compressHTML: false,
     // build: {
     //     assetsPrefix: 'https://page-url'
